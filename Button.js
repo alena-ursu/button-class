178b31a705caa9203585ea2ab7d2a0e6190e5476
alexboisselle-rd <school.alex.boisselle@gmail.com>
2016-04-14 08:18:05 -0400

    Commiting initial JS & index.html

---
+++ Button.js
@@ -0,0 +1,74 @@
+/*
+Button.js
+Author: Alex Boisselle
+Date: 04-12-2016
+Description: Custom button component class for in-class experiment
+Dependencies: jQuery
+Options:
+	container:STRING  jQuery css selector that will be the target container
+	opts:OBJECT 	  JSON object containing CSS styles & button text value
+	callback:FUNCTION function on click
+*/
+
+function Button(container, opts, callback){
+	var component = {
+		init: function(){
+			this.container = container;
+			this.opts = opts;
+			this.callback = callback;
+			this.id = "Button-" + Math.random().toString(36).substring(4);
+
+			this.render();
+		},
+		makeCSS: function(){
+			var css = "#" + this.id + "{"
+						+ "background-color:" + this.opts.backgroundColor + ";"
+						+ "color:" + this.opts.textColor + ";"
+						+ "border: 1px solid " + this.opts.borderColor + ";"
+					+ "}";
+
+			this.css = css;
+		},
+		makeHTML: function(){
+			var html = "<button id='" + this.id + "' class='custom-class'>"
+					 	+ this.opts.text
+				 	 + "</button>";
+
+		 	this.html = html;
+		},
+		render: function(){
+			this.makeCSS();
+			this.makeHTML();
+
+			$(this.container).append('<style>' + this.css + '</style>');
+			$(this.container).append(this.html);
+
+			this.bind();
+		},
+		bind: function(){
+			$('#' + this.id).on('click', this.callback);
+		},
+		destroy: function(){
+			$('#' + this.id).remove();
+
+			delete this;
+		}
+	};
+
+	component.init();
+
+	return component;
+}
+
+$('document').ready(function(){
+
+	var buttonTest = new Button("#button-container", {
+		textColor: "#fff",
+		backgroundColor: "#000",
+		borderColor: "red",
+		text: "Click here!!! Said Carlos."
+	}, function(){
+		console.log("Hello from the black button.");
+	});
+
+});
